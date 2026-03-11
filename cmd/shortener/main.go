a2e3305c1aef1179fff618770f42bd1d8e026174
asbm77 <asbm@bk.ru>
2026-03-11 16:35:49 +0300

    iter1 http serv

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,58 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+var urlMap = make(map[string]string)
+
+func apiPost(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "400 Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	url := req.FormValue("url")
+	if len(url) == 0 {
+		http.Error(res, "400 Bad Request", http.StatusBadRequest)
+	}
+
+	shortUrl := fmt.Sprintf("/%d", len(urlMap)+1)
+	urlMap[shortUrl] = url
+
+	res.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(res, "%s", shortUrl)
+}
+
+func apiGet(res http.ResponseWriter, req *http.Request) {
+	id := req.URL.Path[len("/"):]
+
+	asb := urlMap[id]
+	if asb == "" {
+		http.NotFound(res, req)
+		return
+	}
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		switch req.Method {
+		case http.MethodPost:
+
+			apiPost(res, req)
+		case http.MethodGet:
+			apiGet(res, req)
+		default:
+			http.Error(res, "400 Bad Request", http.StatusBadRequest)
+		}
+	})
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}

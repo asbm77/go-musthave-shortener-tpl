b6423186317b6030724bb6db47bc79470270e8af
asbm77 <asbm@bk.ru>
2026-03-25 17:22:57 +0300

    iter4 flag

--- cmd/shortener/config.go
+++ cmd/shortener/config.go
@@ -9,6 +9,6 @@ var flagShortAddr string
 
 func parseFlags() {
 	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&flagShortAddr, "b", "http://localhost:8080/", "address and port short url")
+	flag.StringVar(&flagShortAddr, "b", "http://localhost:8080", "address and port short url")
 	flag.Parse()
 }
